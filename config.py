ecb4e5287c837f0c07189b4e053710ef8bbbdda6
Josh <jjw.watson2710@gmail.com>
2026-04-29 00:23:35 +0100

    added production vs dev environments

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings
-
+from typing import Literal
 
 class Settings(BaseSettings):
     app_name: str = "WorkPlace Wellness"
     admin_email: str = ""
     items_per_user: int = 50
     default_user_type_id:int = 2
+    environment: Literal['production', 'dev'] = 'dev'
 
 settings = Settings()
